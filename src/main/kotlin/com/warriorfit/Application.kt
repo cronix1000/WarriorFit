d6eaa07b61f9586f8dfc2affa4680f3fd3db82fe
Idan Gomberg <crackenyogs10@gmail.com>
2024-11-06 15:51:05 -0500

    Added controllers folder

---
+++ src/main/kotlin/com/warriorfit/Application.kt
@@ -0,0 +1,12 @@
+package com.warriorfit
+
+import com.warriorfit.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
