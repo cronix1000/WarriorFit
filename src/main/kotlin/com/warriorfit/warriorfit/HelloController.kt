e9dfa2584c736ec376838b1565469b2b91bd3a9e
Idan Gomberg <crackenyogs10@gmail.com>
2024-11-13 00:07:04 -0500

    Few Changes. Started Homepage

--- src/main/kotlin/com/warriorfit/warriorfit/HelloController.kt
+++ src/main/kotlin/com/warriorfit/warriorfit/HelloController.kt
@@ -2,13 +2,21 @@ package com.warriorfit.warriorfit
 
 import javafx.fxml.FXML
 import javafx.scene.control.Label
+//button
+import javafx.scene.control.Button
+import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag
 
 class HelloController {
     @FXML
-    private lateinit var welcomeText: Label
+    public lateinit var createButton: Button
+    public lateinit var loginButton: Button
 
     @FXML
-    private fun onHelloButtonClick() {
-        welcomeText.text = "Welcome to JavaFX Application!"
+    public fun onCreateButtonClick() {
+        createButton.text = "Create Button Clicked"
+    }
+
+    public fun onLoginButtonClick() {
+        loginButton.text = "Login Button Clicked"
     }
 }
