76e2b9fb0ae4a4c728952541f46653563e4f01e3
Ethan Curtis <95894855+UltraSpeedDemon@users.noreply.github.com>
2024-11-14 12:55:17 -0500

    Few Changes. Started Homepage

--- src/main/kotlin/com/warriorfit/warriorfit/HelloController.kt
+++ src/main/kotlin/com/warriorfit/warriorfit/HelloController.kt
@@ -5,11 +5,13 @@ import javafx.scene.control.Label
 //button
 import javafx.scene.control.Button
 import javax.security.auth.login.AppConfigurationEntry.LoginModuleControlFlag
+import kotlin.system.exitProcess
 
 class HelloController {
     @FXML
     public lateinit var createButton: Button
     public lateinit var loginButton: Button
+    public lateinit var exitButton: Button
 
     @FXML
     public fun onCreateButtonClick() {
@@ -18,5 +20,11 @@ class HelloController {
 
     public fun onLoginButtonClick() {
         loginButton.text = "Login Button Clicked"
+    }
+
+    public fun onExitButtonClick() {
+        //close the application
+        exitButton.text = "Exit Button Clicked"
+        exitProcess(0)
     }
 }
